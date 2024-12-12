b2a39b7cb344fc69b7f580fe56c0b943bd90ec7d
Boris Todorov <btodorovtodorov@gmail.com>
2024-12-12 16:55:26 +0200

    search field update

--- src/views/search.js
+++ src/views/search.js
@@ -1,8 +1,10 @@
 import { html, render } from '../../node_modules/lit-html/lit-html.js';
 
 const template = () => html`<section id="search-section">
-    <p id="search-question">Are you interested in something?</p>
-    <input type="text" name="search" id="search-field" placeholder="Search for books or authors..." />
+    <form>
+        <p id="search-question">Are you interested in something?</p>
+        <input type="text" name="search" id="search-field" placeholder="Search for books or authors..." />
+    </form>
 </section>`;
 
 export default async function searchView(ctx) {
