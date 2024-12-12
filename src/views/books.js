bc0c229dc388bb2ac0e44906e1bf7f54923ecb43
Boris Todorov <btodorovtodorov@gmail.com>
2024-12-12 23:06:49 +0200

    update dashboard

--- src/views/books.js
+++ src/views/books.js
@@ -3,21 +3,22 @@ import { getAllBooks } from '../api/books.js';
 
 const template = (books) => html`<!-- Dashboard page -->
     <section id="dashboard">
-        <div class="book">
-            <img
-                src="https://www.ciela.com/media/catalog/product/cache/32bb0748c82325b02c55df3c2a9a9856/p/o/pod-igoto-softpres-9786191516636.jpg"
-                alt="example1"
-            />
-            <h3 class="name">Под Игото</h3>
-            <div class="book-info">
-                <p class="author">Иван Вазов</p>
-            </div>
-            <a class="details-btn" href="#">Details</a>
-        </div>
+        ${books.map(
+            (book) =>
+                html`<div class="book">
+                    <img src="${book.img}" alt="example1" />
+                    <h3 class="name">${book.name}</h3>
+                    <div class="book-info">
+                        <p class="author">${book.author}</p>
+                    </div>
+                    <a class="details-btn" href="#">Details</a>
+                </div>`
+        )}
     </section> `;
 
 export default async function booksView(ctx) {
     const books = await getAllBooks();
+    console.log(books);
 
     render(template(books), document.querySelector('#wrapper main'));
 }
