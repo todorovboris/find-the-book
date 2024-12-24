8e769d842ca98be631f67f8f33f57c166e2ca099
Boris Todorov <btodorovtodorov@gmail.com>
2024-12-24 19:00:49 +0200

    Update navigation.js

--- src/middlewares/navigation.js
+++ src/middlewares/navigation.js
@@ -3,7 +3,6 @@ import { render, html } from '../../node_modules/lit-html/lit-html.js';
 const template = () => html` <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
     <div class="flex lg:flex-1">
         <a href="/" class="-m-1.5 p-1.5">
-            <span class="sr-only">Your Company</span>
             <img class="home-button" src="./images/homebook.png" alt="" />
         </a>
     </div>
