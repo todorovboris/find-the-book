f04c25813e8c15bdf40c8352a62de87ca78baca8
Boris Todorov <btodorovtodorov@gmail.com>
2024-12-25 19:05:32 +0200

    search updates

--- src/views/search.js
+++ src/views/search.js
@@ -1,6 +1,7 @@
 import { html, render } from '../../node_modules/lit-html/lit-html.js';
+import { getAllBooks } from '../api/books.js';
 
-const template = (onSearch) => html`<section id="search-section">
+const template = (onSearch, results, hasSearched) => html`<section id="search-section">
     <form @submit=${onSearch}>
         <p id="search-question">Търсите ли нещо?</p>
         <div class="search-container">
@@ -8,10 +9,23 @@ const template = (onSearch) => html`<section id="search-section">
             <input type="submit" id="search-button" value="Търси" />
         </div>
     </form>
+    ${hasSearched
+        ? html`<section id="results-section">
+              ${results && results.length > 0
+                  ? html` <ul id="results-list">
+                        ${results.map((result) => html`<li>${result.name} от ${result.author}</li>`)}
+                    </ul>`
+                  : html`<p>Няма намерени резултати.</p>`}
+          </section>`
+        : ''}
 </section> `;
 
+let currentResults = [];
+let hasSearched = false;
+
 export default async function searchView(ctx) {
-    render(template(searchSubmitHandler), document.querySelector('#wrapper main'));
+    hasSearched = false;
+    render(template(searchSubmitHandler, currentResults, hasSearched), document.querySelector('#wrapper main'));
 }
 
 async function searchSubmitHandler(e) {
@@ -19,4 +33,14 @@ async function searchSubmitHandler(e) {
 
     const searchInput = document.querySelector('#search-field').value;
     const bookToFind = searchInput.toLowerCase();
+
+    if (searchInput === '') return;
+
+    const books = await getAllBooks();
+    currentResults = books.filter((book) => book.name.toLowerCase().includes(bookToFind) || book.author.toLowerCase().includes(bookToFind));
+
+    hasSearched = true;
+
+    render(template(searchSubmitHandler, currentResults, hasSearched), document.querySelector('#wrapper main'));
+    // document.querySelector('#search-field').value = '';
 }
