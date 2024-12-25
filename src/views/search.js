5a1f1465854ff981dc1abc692aaa77f920dde80d
Boris Todorov <btodorovtodorov@gmail.com>
2024-12-25 12:24:20 +0200

    search functionality update

--- src/views/search.js
+++ src/views/search.js
@@ -3,9 +3,12 @@ import { html, render } from '../../node_modules/lit-html/lit-html.js';
 const template = () => html`<section id="search-section">
     <form>
         <p id="search-question">Търсите ли нещо?</p>
-        <input type="text" name="search" id="search-field" placeholder="Потърси книга или автор..." />
+        <div class="search-container">
+            <input type="text" name="search" id="search-field" placeholder="Потърси книга или автор..." />
+            <input type="submit" id="search-button" />
+        </div>
     </form>
-</section>`;
+</section> `;
 
 export default async function searchView(ctx) {
     render(template(), document.querySelector('#wrapper main'));
