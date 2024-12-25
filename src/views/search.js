45386fd370e68734757d7f61e7a7e82050e1c7fa
Boris Todorov <btodorovtodorov@gmail.com>
2024-12-25 17:13:05 +0200

    Update search.js

--- src/views/search.js
+++ src/views/search.js
@@ -1,7 +1,7 @@
 import { html, render } from '../../node_modules/lit-html/lit-html.js';
 
-const template = () => html`<section id="search-section">
-    <form>
+const template = (onSearch) => html`<section id="search-section">
+    <form @submit=${onSearch}>
         <p id="search-question">Търсите ли нещо?</p>
         <div class="search-container">
             <input type="text" name="search" id="search-field" placeholder="Потърси книга или автор..." />
@@ -11,5 +11,12 @@ const template = () => html`<section id="search-section">
 </section> `;
 
 export default async function searchView(ctx) {
-    render(template(), document.querySelector('#wrapper main'));
+    render(template(searchSubmitHandler), document.querySelector('#wrapper main'));
+}
+
+async function searchSubmitHandler(e) {
+    e.preventDefault();
+
+    const searchInput = document.querySelector('#search-field').value;
+    const bookToFind = searchInput.toLowerCase();
 }
