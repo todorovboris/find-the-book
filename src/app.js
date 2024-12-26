4ea7b523e89dde8b9dcd166174c65d7638274d20
Boris Todorov <btodorovtodorov@gmail.com>
2024-12-26 11:03:25 +0200

    add functionality for authors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import page from '../node_modules/page/page.mjs';
 
 import navigationMiddleware from './middlewares/navigation.js';
+import authorsView from './views/authors.js';
 import booksView from './views/books.js';
 import detailsView from './views/details.js';
 import homeView from './views/home.js';
@@ -12,5 +13,6 @@ page('/', homeView);
 page('/search', searchView);
 page('/books', booksView);
 page('/books/:bookId/details', detailsView);
+page('/authors', authorsView);
 
 page();
---
+++ src/views/authors.js
@@ -0,0 +1,46 @@
+import { html, render } from '../../node_modules/lit-html/lit-html.js';
+import { getAllBooks } from '../api/books.js';
+import page from '../../node_modules/page/page.mjs';
+
+const template = (authorsAndBooks) => html`
+    <section id="authors-section">
+        <h1>Автори и техните книги</h1>
+        ${authorsAndBooks.length > 0
+            ? html`<ul id="authors-list">
+                  ${authorsAndBooks.map(
+                      (author) => html`<li>
+                          <h2>${author.name}</h2>
+                          <ul>
+                              ${author.books.map((book) => html`<li @click=${() => onBookClick(book.id)}>${book.name}</li>`)}
+                          </ul>
+                      </li>`
+                  )}
+              </ul>`
+            : html`<p>Няма намерени автори или книги.</p>`}
+    </section>
+`;
+
+async function fetchAuthorsAndBooks() {
+    const books = await getAllBooks();
+
+    const authorsMap = new Map();
+
+    books.forEach((book) => {
+        if (!authorsMap.has(book.author)) {
+            authorsMap.set(book.author, []);
+        }
+        authorsMap.get(book.author).push({ name: book.name, id: book._id });
+    });
+
+    return Array.from(authorsMap, ([name, books]) => ({ name, books })).sort((a, b) => a.name.localeCompare(b.name));
+}
+
+function onBookClick(bookId) {
+    page.redirect(`/books/${bookId}/details`);
+}
+
+export default async function authorsView(ctx) {
+    const authorsAndBooks = await fetchAuthorsAndBooks();
+
+    render(template(authorsAndBooks), document.querySelector('#wrapper main'));
+}
